2843a69be01bb1d53f375fb20dabc752dd0e2f36
deveshbajpai19 <devesh.bajpai19@gmail.com>
2021-07-16 17:04:16 -0700

    pair RDD - map values

--- src/main/java/com/sparkTutorial/pairRdd/mapValues/AirportsUppercaseProblem.java
+++ src/main/java/com/sparkTutorial/pairRdd/mapValues/AirportsUppercaseProblem.java
@@ -1,5 +1,12 @@
 package com.sparkTutorial.pairRdd.mapValues;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
 public class AirportsUppercaseProblem {
 
     public static void main(String[] args) throws Exception {
@@ -19,5 +26,24 @@ public class AirportsUppercaseProblem {
            ("Wewak Intl", "PAPUA NEW GUINEA")
            ...
          */
+        SparkConf conf = new SparkConf().setAppName("airports-self").setMaster("local[2]");  //locally with 2 cores
+
+        //connection to the spark cluster
+        JavaSparkContext javaSparkContext = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = javaSparkContext.textFile("in/airports.text");
+
+        JavaPairRDD<String, String> pairRDD = lines.mapToPair(getPairFunction());
+
+        JavaPairRDD<String, String> pairRDDUppercase = pairRDD.mapValues(String::toUpperCase);
+
+        pairRDDUppercase.saveAsTextFile("out/airports_uppercase_self.txt");
+    }
+
+    private static PairFunction<String, String, String> getPairFunction() {
+        return s -> {
+            String[] vals = s.split(",");
+            return new Tuple2<>(vals[1],vals[3]);
+        };
     }
 }
