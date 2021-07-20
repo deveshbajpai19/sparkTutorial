09c0aa3020cf00263d230f49b1120d2cd5c5b567
deveshbajpai19 <devesh.bajpai19@gmail.com>
2021-07-20 14:13:33 -0700

    groupByKey usage

--- src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.java
+++ src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.java
@@ -1,5 +1,14 @@
 package com.sparkTutorial.pairRdd.groupbykey;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Map;
+
 public class AirportsByCountryProblem {
 
     public static void main(String[] args) throws Exception {
@@ -18,5 +27,28 @@ public class AirportsByCountryProblem {
            "Papua New Guinea",  ["Goroka", "Madang", ...]
            ...
          */
+
+        SparkConf conf = new SparkConf().setAppName("airports-self").setMaster("local[2]");  //locally with 2 cores
+
+        //connection to the spark cluster
+        JavaSparkContext javaSparkContext = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = javaSparkContext.textFile("in/airports.text");
+
+        JavaPairRDD<String, String> pairRDD = lines.mapToPair(getPairFunction()); //airport country -> airport name
+
+        JavaPairRDD<String, Iterable<String>> pairRDDGrouped = pairRDD.groupByKey(); //airport country -> Iterable (airport names)
+
+        for (Map.Entry<String, Iterable<String>> airports : pairRDDGrouped.collectAsMap().entrySet()) {
+            System.out.println(airports.getKey() + " : " + airports.getValue());
+        }
+
+    }
+
+    private static PairFunction<String, String, String> getPairFunction() {
+        return s -> {
+            String[] vals = s.split(",");
+            return new Tuple2<>(vals[3],vals[1]);
+        };
     }
 }
